f65514ef7efe46fb7ed9a37ba592724bf0df633e
Romaha93 <raboshroman93@gmail.com>
2024-06-28 18:50:04 +0300

    finished task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,32 @@
+'use strict';
 
+const thisForm = document.querySelector(".login-form");
+
+thisForm.addEventListener('submit', hendlerLogin);
+
+function hendlerLogin(event) {
+    
+    event.preventDefault();
+
+    const { email, password } = event.currentTarget.elements;
+
+    const dataForm = {
+        
+        email: email.value.trim(),
+        password: password.value.trim(),
+    
+    };
+
+    event.currentTarget.reset();
+    
+    if (dataForm.email.length <= 0 || dataForm.password.length <= 0) {
+        
+        return alert('All form fields must be filled in');
+    
+    } else {
+        
+        return console.log(dataForm);
+    
+    };
+
+};
