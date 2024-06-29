389bb32451824b4078c0267ce7a9a770f6096bef
Romaha93 <raboshroman93@gmail.com>
2024-06-29 18:19:40 +0300

    finished task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,22 @@
+'use strict';
+
+const changeBtn = document.querySelector('.change-color');
+const valueColor = document.querySelector('.color');
+const boxColor = document.querySelector('.widget');
+
+changeBtn.addEventListener('click', hendlerClick);
+
+function hendlerClick() {
+  boxColor.style.backgroundColor = `${getRandomHexColor()}`;
+  valueColor.textContent =  boxColor.style.backgroundColor;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+
+
+
